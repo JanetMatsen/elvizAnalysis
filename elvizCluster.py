da447a6f670160ee93c3ecb700f5d7a000e7585b
Dave Beck <dacb@uw.edu>
2016-01-16 12:03:36 -0800

    updated normalization but it is about to replaced by scikit-learn norm

--- elvizCluster.py
+++ elvizCluster.py
@@ -6,25 +6,37 @@ import re
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+minRows = 10
+
 sns.set(style="whitegrid")
 
-def read_elviz_csv(filename):
+def readElvizCSV(filename):
     df = pd.read_csv(filename, sep=",", low_memory=False)
     # repalce nans with ""
     df.fillna("", inplace=True)
     return df
 
 # Read in all the .csv files and concatenate them.
-df = read_elviz_csv("Metagenome_9_HOW4.csv");
+df = readElvizCSV("Metagenome_9_HOW4.csv");
 #print(df.head())
 
 # add some useful columns
 df['reads'] = df['Plus reads'] + df['Minus reads']
 df['rpk'] = df['reads']/(df['Length']/1000)
 
+clusterColumns = ['Reference GC', 'rpk', 'Average fold']
+
 # find unique values of taxonomy columns
 dfgb = df.groupby(['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species'])
 for key in dfgb.indices.keys():
     idx = dfgb.indices[key]
     taxRows = df.iloc[idx]
-    print(taxRows.head())
+    if len(taxRows) < minRows:
+        continue
+    # normalize all dimensions to be used in clustering, e.g. GC, coverage, rpk
+    # begin by zero centering the columns
+    #for col in clusterColumns:
+    ##    taxRows[col] -= np.mean(taxRows[col])
+    ##   # normalize by the range or the variance?
+    ##    # for now, variance
+    ##    taxRows[col] /= np.var(taxRows[col])
