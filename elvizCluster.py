04ce717fecdad3d11c8ed7501a05c6c2038e23d0
Dave Beck <dacb@uw.edu>
2016-01-16 12:29:58 -0800

    working prototype without any attempt to parameterize

--- elvizCluster.py
+++ elvizCluster.py
@@ -5,6 +5,12 @@ import pandas as pd
 import re
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
 
 minRows = 10
 
@@ -25,18 +31,52 @@ df['reads'] = df['Plus reads'] + df['Minus reads']
 df['rpk'] = df['reads']/(df['Length']/1000)
 
 clusterColumns = ['Reference GC', 'rpk', 'Average fold']
+clusterColumns = ['Reference GC', 'Average fold']
 
-# find unique values of taxonomy columns
-dfgb = df.groupby(['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species'])
-for key in dfgb.indices.keys():
-    idx = dfgb.indices[key]
-    taxRows = df.iloc[idx]
-    if len(taxRows) < minRows:
-        continue
-    # normalize all dimensions to be used in clustering, e.g. GC, coverage, rpk
-    # begin by zero centering the columns
-    #for col in clusterColumns:
-    ##    taxRows[col] -= np.mean(taxRows[col])
-    ##   # normalize by the range or the variance?
-    ##    # for now, variance
-    ##    taxRows[col] /= np.var(taxRows[col])
+def plotClusters(pdf, X, title):
+    # Black removed and is used for noise instead.
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+
+        xy = X[class_member_mask & core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+
+        xy = X[class_member_mask & ~core_samples_mask]
+        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=6)
+
+    plt.title(title)
+    pdf.savefig()
+    plt.close()
+
+# create a multiage PDF for storing the plots
+with PdfPages('elvizCluster.pdf') as pdf:
+    # find unique values of taxonomy columns
+    dfgb = df.groupby(['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species'])
+    for key in dfgb.indices.keys():
+        idx = dfgb.indices[key]
+        taxRows = df.iloc[idx]
+        if len(taxRows) < minRows:
+            continue
+        # normalize all dimensions to be used in clustering, e.g. GC, coverage, rpk
+        taxRowsClusterColumns = StandardScaler().fit_transform(taxRows[clusterColumns])
+
+        db = DBSCAN(eps=0.3, min_samples=10).fit(taxRowsClusterColumns)
+        core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+        core_samples_mask[db.core_sample_indices_] = True
+        labels = db.labels_
+
+        # Number of clusters in labels, ignoring noise if present.
+        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+        title = ', '.join(key)
+        print(title)
+        print('Estimated number of clusters: %d' % n_clusters_)
+        plotClusters(pdf, taxRowsClusterColumns, title)
