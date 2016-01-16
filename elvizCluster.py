6ff6bb5c87e2d01be7699abdd68577b1fbaf1672
Dave Beck <dacb@uw.edu>
2016-01-16 14:15:07 -0800

    output plot is now in origin feature space, not transformed

--- elvizCluster.py
+++ elvizCluster.py
@@ -13,7 +13,7 @@ from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 
 MIN_ROWS = 20
-DENSITY = 0.4
+DENSITY = 0.45
 MIN_SAMPLES = 10
 
 sns.set(style="whitegrid")
@@ -38,7 +38,7 @@ clusterColumns = ['Average fold', 'Reference GC']
 def plotClusters(pdf, X, title):
     # Black removed and is used for noise instead.
     unique_labels = set(labels)
-    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)), alpha=0.6)
     for k, col in zip(unique_labels, colors):
         if k == -1:
             # Black used for noise.
@@ -48,15 +48,19 @@ def plotClusters(pdf, X, title):
 
         xy = X[class_member_mask & core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
+             markeredgecolor='k', markersize=6)
 
         xy = X[class_member_mask & ~core_samples_mask]
         plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=6)
+             markeredgecolor='k', markersize=3)
 
     plt.title(title)
+    plt.xlabel(clusterColumns[0], fontsize=10)
+    plt.ylabel(clusterColumns[1], fontsize=10)
     pdf.savefig()
     plt.close()
+
+s = StandardScaler()
 
 # create a multiage PDF for storing the plots
 with PdfPages('elvizCluster.pdf') as pdf:
@@ -68,7 +72,7 @@ with PdfPages('elvizCluster.pdf') as pdf:
         if len(taxRows) < MIN_ROWS:
             continue
         # normalize all dimensions to be used in clustering, e.g. GC, coverage, rpk
-        taxRowsClusterColumns = StandardScaler().fit_transform(taxRows[clusterColumns])
+        taxRowsClusterColumns = s.fit_transform(taxRows[clusterColumns])
 
         db = DBSCAN(eps=DENSITY, min_samples=MIN_SAMPLES).fit(taxRowsClusterColumns)
         core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
@@ -84,4 +88,4 @@ with PdfPages('elvizCluster.pdf') as pdf:
         title = ', '.join(key)
         print(title)
         print('Estimated number of clusters: %d' % n_clusters_)
-        plotClusters(pdf, taxRowsClusterColumns, title)
+        plotClusters(pdf, s.inverse_transform(taxRowsClusterColumns), title)
