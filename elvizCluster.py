c894d167838f78c8cff451a62df3b6caf9b2f12c
Dave Beck <dacb@uw.edu>
2016-01-16 13:37:59 -0800

    attempt to set the range manually

--- elvizCluster.py
+++ elvizCluster.py
@@ -35,7 +35,7 @@ df['rpk'] = df['reads']/(df['Length']/1000)
 clusterColumns = ['Reference GC', 'rpk', 'Average fold']
 clusterColumns = ['Average fold', 'Reference GC']
 
-def plotClusters(pdf, X, title):
+def plotClusters(pdf, originalX, title):
     # Black removed and is used for noise instead.
     unique_labels = set(labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
@@ -46,13 +46,18 @@ def plotClusters(pdf, X, title):
 
         class_member_mask = (labels == k)
 
-        xy = X[class_member_mask & core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=14)
-
-        xy = X[class_member_mask & ~core_samples_mask]
-        plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
+        xy = originalX[class_member_mask & core_samples_mask]
+        x = xy[clusterColumns[0]]
+        y = xy[clusterColumns[1]]
+        plt.plot(x, y, 'o', markerfacecolor=col,
              markeredgecolor='k', markersize=6)
+
+        xy = originalX[class_member_mask & ~core_samples_mask]
+        plt.plot(x, y, 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=3)
+
+        plt.xlabel(clusterColumns[0], fontsize=10)
+        plt.ylabel(clusterColumns[1], fontsize=10)
 
     plt.title(title)
     pdf.savefig()
@@ -84,4 +89,4 @@ with PdfPages('elvizCluster.pdf') as pdf:
         title = ', '.join(key)
         print(title)
         print('Estimated number of clusters: %d' % n_clusters_)
-        plotClusters(pdf, taxRowsClusterColumns, title)
+        plotClusters(pdf, taxRows[clusterColumns], title)
