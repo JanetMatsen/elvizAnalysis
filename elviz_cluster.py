20cad7c58e735fdbe0bb7dde27fa510f9b98f01e
Dave Beck <dacb@uw.edu>
2016-01-26 12:50:22 -0800

    revised style to be google python correct (not google R correct, dumb dave)

--- elviz_cluster.py
+++ elviz_cluster.py
@@ -14,27 +14,29 @@ from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
 
+from elviz_utils import IMPORT_DATA_TYPES
+
 MIN_ROWS = 20
 EPS = 0.15
 MIN_SAMPLES = MIN_ROWS
 MAX_AVG_FOLD = 500 # I've seen over 20k, 500 seems to be 2x typical
 DATA_PICKLE = 'data.pkl' # filename of previously parsed data
 
-clusterColumns = ['Average fold', 'Reference GC']
+CLUSTER_COLUMNS = ['Average fold', 'Reference GC']
 
 sns.set(style="whitegrid")
 
 # read in metadata about samples
 meta_info = pd.read_csv("./data/sample_meta_info.tsv", sep="\t")
 
-def readElvizCSV(filename):
-    df = pd.read_csv(filename, sep=",", low_memory=False)
+def read_elviz_CSV(filename):
+    df = pd.read_csv(filename, sep=",", dtype=IMPORT_DATA_TYPES)
     # repalce nans with ""
     df.fillna("", inplace=True)
     return df
 
 
-def plotClusters(pdf, X, title, labels, core_samples_mask, limits):
+def plot_clusters(pdf, X, title, labels, core_samples_mask, limits):
     # Black removed and is used for noise instead.
     unique_labels = set(labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)), alpha=0.6)
@@ -57,22 +59,22 @@ def plotClusters(pdf, X, title, labels, core_samples_mask, limits):
 
     # format the plot
     plt.title(title)
-    plt.xlabel(clusterColumns[0], fontsize=10)
-    plt.ylabel(clusterColumns[1], fontsize=10)
+    plt.xlabel(CLUSTER_COLUMNS[0], fontsize=10)
+    plt.ylabel(CLUSTER_COLUMNS[1], fontsize=10)
     plt.xlim(limits["x"])
     plt.ylim(limits["y"])
     pdf.savefig()
     plt.close()
 
 
-def readElvizCSVs(directory):
-    elvizData = { }
-    elvizFiles = [filename for filename in os.listdir(directory) if ".csv" in filename]
-    for filename in elvizFiles:
+def read_elviz_CSVs(directory):
+    elviz_data = { }
+    elviz_files = [filename for filename in os.listdir(directory) if ".csv" in filename]
+    for filename in elviz_files:
         # read the dataframe from the csv
-        df = readElvizCSV("./data/" + filename)
-        elvizData[filename] = df
-    return elvizData
+        df = read_elviz_CSV("./data/" + filename)
+        elviz_data[filename] = df
+    return elviz_data
 
 
 def main():
@@ -82,20 +84,20 @@ def main():
     if os.path.isfile(DATA_PICKLE):
         print("reading %s for previously parsed data" % DATA_PICKLE)
         with open(DATA_PICKLE, 'rb') as file:
-            elvizData = pickle.load(file)
-            combinedDf = pickle.load(file)
+            elviz_sata = pickle.load(file)
+            combined_df = pickle.load(file)
     else:
         # OK, no pickle found, do it the hard way
         print("reading in all Elviz CSV files")
-        elvizData = readElvizCSVs("./data/")
+        elviz_data = read_elviz_CSVs("./data/")
         # assemble the uber frame
         print("concatenating data frames prior to normalization")
         # create a combined dataframe from all the CSV files
-        combinedDf = pd.concat(elvizData.values())
+        combined_df = pd.concat(elviz_data.values())
         # save the two new objects to a pickle for future use
         with open(DATA_PICKLE, 'wb') as file:
-            pickle.dump(elvizData, file, pickle.HIGHEST_PROTOCOL)
-            pickle.dump(combinedDf, file, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(elviz_data, file, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(combined_df, file, pickle.HIGHEST_PROTOCOL)
 
     # setup plotting limits
     print("determining plotting limits")
@@ -108,34 +110,34 @@ def main():
 
     print("normalizing data prior to clustering")
     # normalize the combined data to retrieve the normalization parameters
-    scaler = StandardScaler().fit(combinedDf[clusterColumns])
+    scaler = StandardScaler().fit(combined_df[CLUSTER_COLUMNS])
     # serializing outputs
 
     print("serially processing files")
-    for filename in elvizData.keys():
-        pdfFilename = filename.replace("csv", "pdf")
+    for filename in elviz_data.keys():
+        pdf_filename = filename.replace("csv", "pdf")
         # skip if the PDF already exists
-        if os.path.isfile("./results/" + pdfFilename):
+        if os.path.isfile("./results/" + pdf_filename):
             print("skiping file %s" % filename)
             continue
         print("processing file %s" % filename)
 
-        df = elvizData[filename]
+        df = elviz_data[filename]
 
         # create a multipage PDF for storing the plots
-        with PdfPages('./results/' + pdfFilename) as pdf:
+        with PdfPages('./results/' + pdf_filename) as pdf:
             # find unique values of taxonomy columns
             dfgb = df.groupby(['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species'])
             for key in dfgb.indices.keys():
                 idx = dfgb.indices[key]
-                taxRows = df.iloc[idx]
-                if len(taxRows) < MIN_ROWS:
+                tax_rows = df.iloc[idx]
+                if len(tax_rows) < MIN_ROWS:
                     continue
                 # normalize all dimensions to be used in clustering, e.g. GC, coverage, rpk
                 # reuse the scaler we created from all of the data for the transform
-                taxRowsClusterColumns = scaler.transform(taxRows[clusterColumns])
+                tax_rows_cluster_columns = scaler.transform(tax_rows[CLUSTER_COLUMNS])
 
-                db = DBSCAN(eps=EPS, min_samples=MIN_SAMPLES).fit(taxRowsClusterColumns)
+                db = DBSCAN(eps=EPS, min_samples=MIN_SAMPLES).fit(tax_rows_cluster_columns)
                 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
                 core_samples_mask[db.core_sample_indices_] = True
                 labels = db.labels_
@@ -149,7 +151,7 @@ def main():
                 title = ', '.join(key)
                 #print(title)
                 #print('Estimated number of clusters: %d' % n_clusters_)
-                plotClusters(pdf, scaler.inverse_transform(taxRowsClusterColumns),
+                plotClusters(pdf, scaler.inverse_transform(tax_rows_cluster_columns),
                         title, labels, core_samples_mask, limits)
 
 
