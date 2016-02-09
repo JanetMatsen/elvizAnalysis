d0a350151ed182f3d68a4ee6c8ed7f20a490d15a
Janet Matsen <janetmatsen@gmail.com>
2016-02-08 16:42:21 -0800

    PEP8 compliant

--- abundance_plot_utils.py
+++ abundance_plot_utils.py
@@ -8,23 +8,32 @@ from abundance_utils import filter_by_abundance
 
 def plot_heatmap(data, high, low, oxy, rep, plot_dir):
     """
-    Plot 
-    """ 
+    Make a heatmap at Genus, using oganisms withing the specified abundance
+    cutoffs.
+
+    :param data: dataframe to pass
+    :param high: highest abundance to include genera for
+    :param low: lowes abundance to include genera for
+    :param oxy: oxygen tension, "Low" or "High"
+    :param rep: replicate (1-4)
+    :param plot_dir: directory to save plots in.
+    :return:
+    """
     # get rid of oxygen levels and replicates if specified.
     if oxy is not 'all':
         print("keep only {} oxygen samples".format(oxy))
-        data = data[data['oxy']==oxy]
+        data = data[data['oxy'] == oxy]
     if rep is not 'all':
         print("keep only replicate levels:", rep)
-        data = data[data['rep'].isin(rep)]        
-    data = filter_by_abundance(data=data, 
-                                column='abundance', 
-                                high=high, low=low)
+        data = data[data['rep'].isin(rep)]
+    data = filter_by_abundance(data=data,
+                               column='abundance',
+                               high=high, low=low)
     data['facet_replicate'] = 'replicate ' + data['rep'].astype(str)
-    
+
     # make height of the plot a function of the number of rows (Genera):
     num_data_rows = len(data['Genus'].unique())
-    plot_size = 2 +  num_data_rows/7
+    plot_size = 2 + num_data_rows / 7
     plot_aspect = 2
     if num_data_rows > 6:
         plot_aspect = .85
@@ -32,37 +41,40 @@ def plot_heatmap(data, high, low, oxy, rep, plot_dir):
         plot_aspect = .65
     if num_data_rows > 9:
         plot_aspect = .6
-    
-    def facet_heatmap(data, color, **kws):
-        data = data.pivot(index='Genus', 
-                          columns='week', 
+
+    def facet_heatmap(data, **kws):
+        data = data.pivot(index='Genus',
+                          columns='week',
                           values='abundance')
-        sns.heatmap(data, cmap="YlGnBu", **kws)  # <-- Pass kwargs to heatmap  cmap used to be 'Blue'
-    
+        # Pass kwargs to heatmap  cmap used to be 'Blue'
+        sns.heatmap(data, cmap="YlGnBu", **kws)
+
     with sns.plotting_context(font_scale=7):
-        g = sns.FacetGrid(data, col='facet_replicate', 
+        g = sns.FacetGrid(data, col='facet_replicate',
                           size=plot_size, aspect=plot_aspect)
-    
-    cbar_ax = g.fig.add_axes([.92, .3, .02, .4])  # <-- Create a colorbar axes
-    
+
+        # Create a colorbar axes
+    cbar_ax = g.fig.add_axes([.92, .3, .02, .4])
+
     g = g.map_dataframe(facet_heatmap,
-                        cbar_ax=cbar_ax,
-                        vmin=0, vmax=max(data.abundance))  # <-- Specify the colorbar axes and limits
-    
-    g.set_titles(col_template="{col_name}", 
-                 fontweight='bold', fontsize=18)
+                        cbar_ax=cbar_ax, vmin=0,
+                        # Specify the colorbar axes and limits
+                        vmax=max(data.abundance))
+
+    g.set_titles(col_template="{col_name}", fontweight='bold', fontsize=18)
     g.set_axis_labels('week')
-    g.fig.subplots_adjust(right=.9)  # <-- Add space so the colorbar doesn't overlap the plot
-    
+
+    # Add space so the colorbar doesn't overlap the plot
+    g.fig.subplots_adjust(right=.9)
+
     # add a supertitle.
     # source: http://stackoverflow.com/questions/29813694/how-to-add-a-title-to-seaborn-facet-plot
     plt.subplots_adjust(top=0.80)
-    supertitle = str(low) + ' < abundance < ' + str(high) + ', {} oxygen'.format(oxy) 
-    #g.fig.suptitle(supertitle, top=0.9, size=14)
-    
+    supertitle = str(low) + ' < abundance < ' + str(
+        high) + ', {} oxygen'.format(oxy)
+    # g.fig.suptitle(supertitle, top=0.9, size=14)
+
     # write a filename and save.
-    filename = oxy + "_oxygen--{0}_to_{1}_abundance".format(low,high)
+    filename = oxy + "_oxygen--{0}_to_{1}_abundance".format(low, high)
     print('filename:', filename)
     g.savefig(plot_dir + filename + '.pdf')
-
-
