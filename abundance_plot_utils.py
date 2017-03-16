6d3d187f6f2b5649e097b6d44463a560a883e047
Janet Matsen <janetmatsen@gmail.com>
2017-03-16 11:33:53 -0700

    LaTeX sans-serif font

--- abundance_plot_utils.py
+++ abundance_plot_utils.py
@@ -13,6 +13,14 @@ rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 # Need to use LaTeX to get italic fonts.
 rc('text', usetex=True)
 rc('font', family='sans-serif')
+# http://stackoverflow.com/questions/2537868/sans-serif-math-with-latex-in-matplotlib
+mpl.rcParams['text.latex.preamble'] = [
+       r'\usepackage{siunitx}',   # i need upright \micro symbols, but you need...
+       r'\sisetup{detect-all}',   # ...this to force siunitx to actually use your fonts
+       r'\usepackage{helvet}',    # set the normal font here
+       r'\usepackage{sansmath}',  # load up the sansmath so that math -> helvet
+       r'\sansmath'               # <- tricky! -- gotta actually tell tex to use!
+]
 
 
 def axd_portrait(axs):
